5ec1202e13a83dc4b96777f5d29f53d06275edf7
Sachkirat Singh <sachkiratsingha@gmail.com>
2021-01-12 19:49:29 +0530

    added pig

--- sketch.js
+++ sketch.js
@@ -5,6 +5,7 @@ const Bodies = Matter.Bodies;
 var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
+var pig;
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -36,6 +37,8 @@ function setup(){
 
     bird = new Bird(100,100);
 
+    pig = new Pig(200,200);
+
 }
 
 function draw(){
@@ -61,4 +64,5 @@ function draw(){
 
     bird.display();
     platform.display();
+    pig.display();
 }
