ff3324c213c9cd1e3f2978d24c43711420cfd4aa
Sachkirat Singh <sachkiratsingha927@gmail.com>
2021-01-12 19:56:23 +0530

    addedBird

--- sketch.js
+++ sketch.js
@@ -6,6 +6,7 @@ var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
 var pig;
+var bird1;
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -36,6 +37,7 @@ function setup(){
     log5 = new Log(870,120,150, -PI/7);
 
     bird = new Bird(100,100);
+    bird1 = new Bird(100,200);
 
     pig = new Pig(200,200);
 
@@ -65,4 +67,6 @@ function draw(){
     bird.display();
     platform.display();
     pig.display();
+
+    bird1.display();
 }
